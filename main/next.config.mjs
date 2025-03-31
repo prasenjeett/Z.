78326732c983bd6588a7eadabf67b559d19ae006
Prasenjeet <prasenjeetz27@gmail.com>
2025-03-31 13:10:45 +0600

    Update next.config.mjs

--- main/next.config.mjs
+++ main/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export
+  basePath: "/main", // Change to your repo name
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
